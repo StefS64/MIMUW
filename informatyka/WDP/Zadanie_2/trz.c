d9feb3ff3f64d9495004631027cfe71a98b14097
StefS64 <stefan.swierczewski@gmail.com>
2023-11-16 11:12:26 +0100

    halo

---
+++ informatyka/WDP/Zadanie_2/trz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int* dp_max(int *T){
+
+}
+int input(int *T){
+
+}
+int cout(int *T,int n){
+   for(int i = 0; i < n; i++){
+      printf("%d ",T[i]);
+
+   }printf("\n");
+}
+int main(){
+
+}
